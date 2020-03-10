800de8cfef6f2f53c8f4982b6a7aaf827765a5ee
Guilherme Vogt <vogt.guilherme@gmail.com>
2020-03-10 09:56:00 -0300

    Detecção de trigger via componente

---
+++ Assets/Scripts/Multiplayer/PlayerDetection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerDetection : NetworkBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //Cria uma variável temporária do tipo NetworkIdentity
+        NetworkIdentity identity;
+        //Tenta acessar o componente "NetworkIdentity" do objeto que ativou o trigger
+        identity = other.GetComponent<NetworkIdentity>();
+        //Se o valor da variável não for null...
+        if(identity != null)
+        {
+            //Mostrar esta mensagem
+            Debug.Log("Colidiu com outro NetworkIdentity " + other.gameObject.name);
+        }
+        //Se for null...
+        else
+        {
+            //Mostrar esta mensagem
+            Debug.Log("Não tem NetworkIdentity");
+        }
+    }
+}
